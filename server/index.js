424dc9ba4f480457d3b42c5b99e3ddb9dd73a3b4
equalsAndy <equalsandy@gmail.com>
2024-12-03 15:49:12 -0700

    fixed callback url

--- server/index.js
+++ server/index.js
@@ -81,7 +81,7 @@ passport.use(
       domain: process.env.AUTH0_DOMAIN,
       clientID: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      callbackURL: 'http://localhost:5001/auth/callback', // Server's callback URL
+      callbackURL: process.env.DEFAULT_RETURN+ '/auth/callback', // Server's callback URL
     },
     (accessToken, refreshToken, extraParams, profile, done) => {
       return done(null, profile);
