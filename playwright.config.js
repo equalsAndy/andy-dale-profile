1a4628b79661102152f14bd49c407409a129e982
equalsAndy <equalsandy@gmail.com>
2024-12-30 14:30:18 -0500

    updates

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,14 @@
+const { defineConfig } = require('@playwright/test');
+
 module.exports = defineConfig({
-    testDir: './pr_tests', // Specify Playwright test directory
-    testMatch: ['**/*.e2e.js'], // Match files with .e2e.js extension
-    timeout: 30 * 1000,
-    retries: 1,
-    use: {
-      headless: true,
-      baseURL: 'http://localhost:3003',
-      screenshot: 'on',
-      trace: 'on',
-    },
-  });
+  testDir: './pr_tests',
+  testMatch: ['**/*.e2e.js'], // Match files with .e2e.js extension
+  timeout: 30 * 1000,
+  retries: 1,
+  use: {
+    headless: true,
+    baseURL: 'http://localhost:3003',
+    screenshot: 'on',
+    trace: 'on',
+  },
+});
